a31b0a98f7f5661b25fe69c0354fc1fc1ac98049
Aldrich Reboja <aktreboja@gmail.com>
2022-02-08 23:58:13 -0800

    2/8: Implemented feature that allows for fetch NBA headshots of players

--- pages/players/[player].js
+++ pages/players/[player].js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useRouter } from 'next/router'
-import Axios from 'axios'
+import { useRouter } from 'next/router';
+import Axios from 'axios';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
 
-import styles from './Players.module.css'
+import styles from './Players.module.css';
+import Table from 'react-bootstrap/Table';
 
 
 
@@ -21,7 +22,7 @@ import styles from './Players.module.css'
  * @param {*} param0 
  * @returns 
  */
-const Player = ({ playerInfo, playerAverages }) => {    
+const Player = ({ playerInfo, playerAverages, playerMeta }) => {    
     let router = useRouter()
     let { player } = router.query
     let { 
@@ -32,22 +33,42 @@ const Player = ({ playerInfo, playerAverages }) => {
         position
     } = playerInfo
 
+    let {playerID} = playerMeta
+    console.log(playerID)
+
+    let playerImage = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${playerID}.png`
+
     let PlayerComponent = (
         <Card border = "dark" className = {styles.Card}>
              <div style ={{width: '50px', height: '50px', borderRadius: '50%', background: 'black'}}></div>
+             <img className = {styles.playerImage} src = {playerImage} alt = {`${first_name} ${last_name}`}/> 
             <Card.Title>{ first_name } { last_name } </Card.Title>
             <Card.Text>{ position }</Card.Text>
-            <ListGroup>
-                <ListGroup.Item>Games Played</ListGroup.Item>
-            </ListGroup>
+
         </Card>
     )
-    console.log({playerAverages})
+    
+    // Table headers for the season averages.
+    let avgHeaders = ['Points', 'FGM', 'FGA', '3pt-FGM', '3pt-FGA', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TO', 'PF',  'FG PCT', '3PT PCT', 'FT PCT']
+
+    let averagesCompoent = avgHeaders.map((header, key) => ( <th key = {key}>{ header }</th>))
+    console.log(playerAverages)
 
     return (
         <div> 
             
             { PlayerComponent }
+            <h1>Offensive Stats</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        { averagesCompoent }
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </Table>
         </div>
     )
 }
@@ -55,14 +76,33 @@ const Player = ({ playerInfo, playerAverages }) => {
 
 export async function getServerSideProps(context) {
     let player = context.params.player
+    let playerFirstName = console.log(player.split('_')[0])
+    
+  
 
+    // Get the player's Data
     let response = await Axios.get(`https://www.balldontlie.io/api/v1/players?search=${player}`)
     let data = response.data
     let playerInfo = data.data[0]
 
+    // Get the player's current sesason averages
     let seasonAvgResponse = await Axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${playerInfo.id}`)
     let playerAverages = seasonAvgResponse.data.data[0]
-    return {props : { playerInfo, playerAverages }}
+ 
+
+    // 
+    let playerImageData = await Axios.get('http://data.nba.net/data/10s/prod/v1/2021/players.json')
+    let playersResponse = playerImageData.data
+    let playersArray = playersResponse.league.standard
+
+    let playerIndex = playersArray.findIndex(obj => obj.lastName == 'Morant')
+    let playerId = playersArray[playerIndex].personId
+
+    let playerMeta = {
+        playerID: playerId
+    }
+
+    return {props : { playerInfo, playerAverages, playerMeta }}
 }
 
 export default Player;
