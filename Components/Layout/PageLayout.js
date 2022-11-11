d0d6a98446d959b06b5caca18dbbcc7c9d00d4dc
Aldrich Reboja <aktreboja@gmail.com>
2022-11-10 21:46:06 -0800

    Testing branch push

--- Components/Layout/PageLayout.js
+++ Components/Layout/PageLayout.js
@@ -3,6 +3,7 @@ import Navbar from '../Layout/Navbar'
 
 export default function PageLayout({ children }) {
   return <>
+    {/* Testing this for pull request */}
        <Navbar /> 
       { children }
   </>;
