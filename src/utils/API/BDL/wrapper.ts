623da5e247801d757070f3adf22b2367a401b67a
Aldrich Reboja <aktreboja@gmail.com>
2024-03-25 22:41:27 -0700

    Error handling for build

--- src/utils/API/BDL/wrapper.ts
+++ src/utils/API/BDL/wrapper.ts
@@ -1,10 +1,10 @@
 export const make_bdl_api_request = async (endpoint: string, params?: Record<string, string>) => {
     try {
         const queryString = params ? `?${new URLSearchParams(params)}` : '';
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/bdl${endpoint}${queryString}`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/bdl${endpoint}${queryString}`, );
 
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}, Message: ${response.text}`);
         }
 
         return await response.json();
