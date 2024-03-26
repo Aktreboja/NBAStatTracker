040109470eba92c2de7f4f0406f6b721069e6e4c
Aldrich Reboja <aktreboja@gmail.com>
2024-03-25 17:46:02 -0700

    Linting / build errors

--- src/Components/Team/TeamCard/index.tsx
+++ src/Components/Team/TeamCard/index.tsx
@@ -37,5 +37,3 @@ export default function TeamCard({ team }: {team: Team}) {
     </Link>
   )
 }
-
-
--- src/Components/Team/TeamUpcomingSchedule.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react'
-import Axios from 'axios'
-
-export default function TeamUpcomingSchedule() {
-
-  return (
-    <div>TeamUpcomingSchedule</div>
-  )
-}
