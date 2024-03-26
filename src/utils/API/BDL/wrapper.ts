06674b16b218eecd348080c7aa5716e3133a1318
Aldrich Reboja <aktreboja@gmail.com>
2024-03-25 22:45:26 -0700

    Adjustments to error handling

--- src/utils/API/BDL/wrapper.ts
+++ src/utils/API/BDL/wrapper.ts
@@ -1,10 +1,16 @@
 export const make_bdl_api_request = async (endpoint: string, params?: Record<string, string>) => {
     try {
         const queryString = params ? `?${new URLSearchParams(params)}` : '';
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/bdl${endpoint}${queryString}`, );
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/bdl${endpoint}${queryString}`);
 
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}, Message: ${response.text}`);
+            let errorMessage = `HTTP error! status: ${response.status}`;
+            // Extract the error message from the response if available
+            const responseBody = await response.text();
+            if (responseBody) {
+                errorMessage += `, Message: ${responseBody}`;
+            }
+            throw new Error(errorMessage);
         }
 
         return await response.json();
