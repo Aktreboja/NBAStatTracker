0824d4c8a00100df91737e10a5905f3315c0456d
Aldrich Reboja <aktreboja@gmail.com>
2024-03-26 17:13:14 -0700

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 
+const dotenvExpand = require('dotenv-expand');
+
+dotenvExpand.expand({ parsed: {...process.env } })
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
